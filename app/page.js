003d12ac7ce62ce6a0d32039ff781c490f97d56e
Edgar Lindo <elindo586@gmail.com>
2025-08-05 10:42:36 -0500

    first

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="main">
+      <p>You found this empty page!</p> <br />
+      <p>
+        {" "}
+        Go to <a href="https://www.tu.biz">main site</a>.
+      </p>
+    </div>
+  );
+}
---
+++ app/table/route.js
@@ -0,0 +1,23 @@
+import { neon } from "@neondatabase/serverless";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+	const sql = neon(`${process.env.DATABASE_URL}`);
+
+	try {
+		const result = await sql`
+			CREATE TABLE test1 (
+				email varchar(255),
+				campId varchar(255),
+				date varchar(255)
+			
+			);
+		`;
+
+		return NextResponse.json({ result }, { status: 200 });
+
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+}
