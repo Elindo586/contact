1e596f6bc677ecd2e44e9d054acd54dfff4efda5
Edgar Lindo <elindo586@gmail.com>
2025-08-05 12:38:31 -0500

    eamil2

--- app/api/sendgrid/route.js
+++ app/api/sendgrid/route.js
@@ -5,15 +5,15 @@ import { NextResponse } from "next/server";
 console.log("testing");
 
 // Helper to parse multipart/form-data using formidable with a Promise
-const parseForm = async (req) =>
-	new Promise((resolve, reject) => {
-		const form = new IncomingForm();
+// const parseForm = async (req) =>
+// 	new Promise((resolve, reject) => {
+// 		const form = new IncomingForm();
 
-		form.parse(req, (err, fields, files) => {
-			if (err) return reject(err);
-			resolve(fields);
-		});
-	});
+// 		form.parse(req, (err, fields, files) => {
+// 			if (err) return reject(err);
+// 			resolve(fields);
+// 		});
+// 	});
 
 export async function POST(req) {
 	try {
@@ -24,37 +24,37 @@ export async function POST(req) {
 		const text = formData.get("text");
 		const html = formData.get("html");
 
-		
+	
 
-// 		if (typeof from !== "string" || from.trim() === "")
-// 			throw new Error("from field was invalid"); // something like that // Format timestamp to Chicago time
+		if (typeof from !== "string" || from.trim() === "")
+			throw new Error("from field was invalid"); // something like that // Format timestamp to Chicago time
 
-// 		const now = new Date();
-// 		const chicagoTime = now.toLocaleString("en-US", {
-// 			timeZone: "America/Chicago",
-// 			hour12: true,
-// 		}); // Connect to Neon/Postgres
+		const now = new Date();
+		const chicagoTime = now.toLocaleString("en-US", {
+			timeZone: "America/Chicago",
+			hour12: true,
+		}); // Connect to Neon/Postgres
 
-// 		const sql = neon(process.env.DATABASE_URL); // Insert parsed email into your table
+		const sql = neon(process.env.DATABASE_URL); // Insert parsed email into your table
 
-// 		const result = await sql`INSERT INTO edgar_teams (
-//         from_email,
-//         to_email,
-//         subject,
-//         text_body,
-//         html_body,
-//         timestamp
-//       )
-//      
-//       VALUES (
-//         ${from},
-//         ${to},
-//         ${subject},
-//         ${text},
-//         ${html},
-//         ${chicagoTime}
-//       );`;
-// 		console.log("✅ Email inserted:", result);
+		const result = await sql`INSERT INTO edgar_teams (
+        from_email,
+        to_email,
+        subject,
+        text_body,
+        html_body,
+        timestamp
+      )
+     
+      VALUES (
+        ${from},
+        ${to},
+        ${subject},
+        ${text},
+        ${html},
+        ${chicagoTime}
+      );`;
+		console.log("✅ Email inserted:", result);
 
 		return NextResponse.json({ message: `Email received ${to}` }, { status: 200 });
 	} catch (error) {
