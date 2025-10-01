da4763f41747ce254d2b5b1aefe133101b9a15c3
Edgar Lindo <elindo586@gmail.com>
2025-10-01 13:32:24 -0500

    test

--- app/api/sendgrid/route.js
+++ app/api/sendgrid/route.js
@@ -56,7 +56,7 @@ export async function POST(req) {
     // Configure email message
     const msg = {
       to: "info@tu.biz", // Set to specified recipient
-      from: "edgar@teams.tu.biz", // Verified sender
+      from: to, // Verified sender
       replyTo: from,
       subject: subject,
       custom_args: {"teams": "teams.tu.biz"}, // Kept as custom_args
